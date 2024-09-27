63244d7f137f1d569a32f084a97a1d0a316d10e2
Stephen Kiazyk <s_kiazyk@hotmail.com>
2024-09-27 10:57:25 -0700

    Add convenience method for configuring admin form
    It is generally simpler to just use `AsyncFileField` where appropriate,
    however in cases where it is not possible to adjust the model's field
    type for whatever reason, this can be used in the `ModelAdmin`'s
    `formfield_for_dbfield` method to achieve the same result.
    
    E.g.
    ```
    class CustomModelAdmin(admin.ModelAdmin):
        def formfield_for_dbfield(self, db_field, **kwargs):
            kwargs = make_resumable_admin_file_fields(db_field, ['file'], **kwargs)
            return super(AttachmentAdmin, self).formfield_for_dbfield(db_field, **kwargs)
    ```

--- admin_async_upload/fields.py
+++ admin_async_upload/fields.py
@@ -4,6 +4,20 @@ from django.forms import fields
 from .widgets import ResumableAdminWidget
 
 
+def make_resumable_admin_file_fields(db_field, fieldnames, **kwargs):
+    if db_field.name in fieldnames:
+        defaults = {'form_class': FormResumableFileField, }
+        if db_field.model:
+            defaults['widget'] = ResumableAdminWidget(
+                attrs={
+                    'model': db_field.model,
+                    'field_name': db_field.name
+                }
+            )
+        kwargs.update(defaults)
+    return kwargs
+
+
 class FormResumableFileField(fields.FileField):
     widget = ResumableAdminWidget
 
